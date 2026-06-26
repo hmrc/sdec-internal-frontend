67ffebe11f196ddc4a7ba699101797df1a99732a
OltionF <83361413+OltionF@users.noreply.github.com>
2026-06-26 15:50:46 +0100

    SDEC-45 Modify page to reflect acceptance criterea more accurately

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -32,6 +32,6 @@ class IndexController @Inject() (
     with I18nSupport {
 
   def onPageLoad(): Action[AnyContent] = identify { implicit request =>
-    Ok(view(request.userId))
+    Ok(view(request.userId, "workspace"))
   }
 }
