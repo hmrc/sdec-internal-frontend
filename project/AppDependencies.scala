5566e93e04236d2b72dc1f0817e4a4c1f017268e
ramindur.singh <282824933+ramindursingh@users.noreply.github.com>
2026-09-07 15:37:17 +0100

    Updated app dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion  = "10.7.0"
-  private val hmrcMongoVersion  = "2.12.0"
+  private val hmrcMongoVersion  = "2.13.0"
   private val hmrcPlayFEVersion = "12.32.1"
   private val scalaCheckVersion = "3.2.17.0"
 
